6407c7f1c425aaaec4d507ffc6c5d3b8860f6fd1
jhay-Ben <jamesbentil14@gmail.com>
2024-05-18 17:13:17 +0000

    Completed Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -2,5 +2,19 @@ function processArray(arr) {
     return arr.map(num => (num % 2 === 0 ? num * num : num * 3));
    }
    
-   
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, i) => (numbers[i] % 2 === 0 ? str.toUpperCase() : str.toLowerCase()));
+    }
+
+const arr = [10, 15, 44, 57, 19];
+const strings = ['James', 'Bentil', 'Is', 'The', 'Best'];
+
+
+const processedArr = processArray(arr);
+const formattedStrings = formatArrayStrings(strings, processedArr);
+
+console.log(processedArr); 
+console.log(strings);
+console.log(formattedStrings);
+          
  
